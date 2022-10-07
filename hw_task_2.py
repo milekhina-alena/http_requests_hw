ce084ecdad6863436e4b8ff994c1559d91e0ab4d
Alena Milekhina <milekhina.alena@gmail.com>
2022-10-07 18:21:48 +0800

    task_2 update

--- hw_task_2.py
+++ hw_task_2.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 
 class YaUploader:
@@ -8,37 +9,26 @@ class YaUploader:
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'Authorization': f'0Auth {self.token}'
+            'Authorization': f'OAuth {self.token}'
         }
 
     def _get_upload_link(self, file_path):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': file_path, 'overwrite': 'true'}
+        params = {'path': file_path, 'overwrite': True}
         response = requests.get(upload_url, headers=headers, params=params)
         return response.json()
 
-    # def upload(self, file_path: str):
-    #     url = 'https://dev.yandex.net/disk-polygon/?lang=ru&tld=ru#!/v147disk47resources/PublishResource'
-    #     headers = self.get_headers()
-    #     params = {'path': file_path, 'overwrite': 'true'}
-    #     pesponse = requests.put()
-    #     pass
+    def upload(self, file_path):
+        href = self._get_upload_link(file_path=file_path).get('href', '')
+        with open(file_path, 'rb') as file:
+            requests.put(href, files={'file': file})
 
-    def upload_file_to_disk(self, file_path, filename):
-        href = self._get_upload_link(file_path=file_path).get("href", "")
-        response = requests.put(href, data=open(filename, 'rb'))
-        response.raise_for_status()
-        if response.status_code == 201:
-            print("Success")
+my_token = 'y0_AgAAAAA8HMbLAAh6dQAAAADQrn3L4JugKVPGS1-uQCFLfKLxhqG5JRU'
 
-my_token = 'y0_AgAAAAA8HMbLAADLWwAAAADQWpZPW-W6lf4OTJqmME1qqsxP9UR6UDk'
-
-# Netology/test   test.txt
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
+    path_to_file = 'files/practice.txt'
     token = my_token
     uploader = YaUploader(token)
-    result = uploader.upload_file_to_disk('Netology/test', 'test.txt')
-    print(result)
+    result = uploader.upload(path_to_file)
